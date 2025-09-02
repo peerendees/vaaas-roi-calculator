f52be22da3a9e534ace38b71ff1ec8243d56c152
peerendees <kunkel@h-pcn.de>
2025-09-02 14:28:54 +0200

    🚀 Deploy HTML-Version des ROI-Calculators
    - Vite Config angepasst für HTML-Deployment
    - React-Template temporär deaktiviert
    - HTML/CSS/JS Version wird nun deployed
    - Bereitet Migration zu React vor

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // Keine React-Plugins - deploye die HTML-Version
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  }
 })
